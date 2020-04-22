bc0646cf1d10be7796bab156aefe1506146d2050
JWest2323 <jwest23@ufl.edu>
2020-04-22 17:12:55 -0400

    get curr location and center on last marker working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, OnInit } from "@angular/core";
-import { MapInfoWindow, MapMarker } from "@angular/google-maps";
+import { MapInfoWindow, MapMarker, GoogleMap } from "@angular/google-maps";
 
 @Component({
   selector: "my-app",
@@ -9,13 +9,22 @@ import { MapInfoWindow, MapMarker } from "@angular/google-maps";
 export class AppComponent implements OnInit {
   name = "Angular";
 
+  @ViewChild(GoogleMap) map: GoogleMap;
   @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow;
 
-  center = { lat: 24, lng: 12 };
+  center: google.maps.LatLngLiteral;
   markerOptions = { draggable: false };
   markerPositions: google.maps.LatLngLiteral[] = [];
   zoom = 10;
   display?: google.maps.LatLngLiteral;
+
+  // marker = new google.maps.Marker({
+  //   map: map,
+  //   position: {lat: -34.397, lng: 150.644},
+  //   icon: {
+  //     url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+  //   }
+  // });
 
   getCurrentLocation(){
     if(navigator.geolocation) {
@@ -24,10 +33,18 @@ export class AppComponent implements OnInit {
         const latitude = position.coords.latitude;
         const latLng = {lat: latitude, lng: longitude};
         this.center = latLng; 
+        this.markerPositions.push(latLng);
         // console.log(this.center);
         
       })
     }
+  }
+
+  centerOnMarker(marker: google.maps.LatLngLiteral) {
+    const lat = marker.lat;
+    const lng = marker.lng;
+    const latLng = {lat: lat, lng: lng};
+    this.center = latLng;
   }
 
   ngOnInit(){
@@ -37,6 +54,8 @@ export class AppComponent implements OnInit {
 
   addMarker(event: google.maps.MouseEvent) {
     this.markerPositions.push(event.latLng.toJSON());
+    console.log(this.markerPositions[this.markerPositions.length - 1]);
+    
   }
 
   move(event: google.maps.MouseEvent) {
