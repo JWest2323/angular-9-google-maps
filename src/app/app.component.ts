908165c418fb63b23a8238dd9e8ff2dac9189a32
JWest2323 <jwest23@ufl.edu>
2020-04-29 13:36:28 -0400

    running but google maps underneath the sidenav (fix needed)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component, ViewChild, OnInit } from "@angular/core";
 import { MapInfoWindow, MapMarker, GoogleMap } from "@angular/google-maps";
 
+
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
   name = "Angular";
+  opened = false;
 
-  @ViewChild(GoogleMap) map: google.maps.Map;
+  @ViewChild('map') map: google.maps.Map;
   @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow;
 
   center: google.maps.LatLngLiteral;
@@ -17,7 +19,7 @@ export class AppComponent implements OnInit {
   markerPositions: google.maps.LatLngLiteral[] = [];
   zoom = 10;
   display?: google.maps.LatLngLiteral;
-
+  currentLocationInfo: google.maps.LatLngLiteral;
   myMarker = new google.maps.Marker({
     // position: {lat: -34.397, lng: 150.644},
     icon: {
@@ -31,7 +33,7 @@ export class AppComponent implements OnInit {
         const longitude = position.coords.longitude;
         const latitude = position.coords.latitude;
         const latLng = {lat: latitude, lng: longitude};
-        this.myMarker.setIcon("http://maps.google.com/mapfiles/ms/icons/blue-dot.png");
+        this.currentLocationInfo = latLng;
         this.myMarker.setMap(this.map);
         this.myMarker.setPosition(latLng);
         this.center = latLng; 
@@ -47,17 +49,14 @@ export class AppComponent implements OnInit {
     this.center = this.myMarker.getPosition().toJSON();
   }
 
-  centerOnMarker(marker: google.maps.LatLngLiteral) {
-    const lat = marker.lat;
-    const lng = marker.lng;
-    const latLng = {lat: lat, lng: lng};
+  centerOnMarker(event: google.maps.MouseEvent) {
+    const latLng = event.latLng.toJSON();
     this.center = latLng;
   }
 
   ngOnInit(){
     this.getCurrentLocation();
   }
-
 
   addMarker(event: google.maps.MouseEvent) {
     this.markerPositions.push(event.latLng.toJSON());
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import {GoogleMapsModule} from '@angular/google-maps';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, GoogleMapsModule ],
+  imports:      [ BrowserModule, FormsModule, GoogleMapsModule, BrowserAnimationsModule, MatSidenavModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
